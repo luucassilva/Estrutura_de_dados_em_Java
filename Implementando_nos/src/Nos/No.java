5e22983193f6c5d4b53d542c8f03c7624dd9387a
Lucas Silva <lucascavalcante@hotmail.com>
2021-08-18 21:51:23 -0300

    Implementando Conceito de nó e Encadeamento de nó

---
+++ Implementando_nos/src/Nos/No.java
@@ -0,0 +1,35 @@
+package Nos;
+
+public class No <T> {
+
+    private T conteudo;
+    private No <T> proximoNo;
+
+    public No( T conteudo) {
+        this.proximoNo = null;
+        this.conteudo = conteudo;
+    }
+
+    public T getConteudo() {
+        return conteudo;
+    }
+
+    public No <T> getProximoNo() {
+        return proximoNo;
+    }
+
+    public void setConteudo( T conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public void setProximoNo(No proximoNo) {
+        this.proximoNo = proximoNo;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "conteudo='" + conteudo + '\'' +
+                '}';
+    }
+}
---
+++ Implementando_nos/src/module-info.java
@@ -0,0 +1,2 @@
+module Implementando_nos {
+}
